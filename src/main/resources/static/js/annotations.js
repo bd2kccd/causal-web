91e8cc023f666d35e1c53763179a1c1bd50d0b74
Mark Silvis <masilvis22@gmail.com>
2016-05-25 18:34:02 -0400

    Retrieves tokens from annotations API
    Access token is stored as cookie. Refresh token is stored in session storage

--- src/main/resources/static/js/annotations.js
+++ src/main/resources/static/js/annotations.js
@@ -2,32 +2,12 @@ const apiURL = "http://localhost:8000/api";
 const tokenURL = apiURL+"/oauth/token";
 
 function getAnnotationsToken() {
-    const client = "curl";
+    const client = "causal-web";
     const clientPassword = "";
     const grantType = "password";
     const username = document.getElementById("login").username.value;
     const password = document.getElementById("login").password.value;
 
-    console.log("fetching annotations token for " + "username: " + username);
-
-    console.log("groups test");
-    $.ajax({
-        url: apiURL+"/groups",
-        type: 'get',
-        headers: {
-            'Authorization': 'Bearer 8dcd34a8-7af8-457f-a639-757281c9179a',
-        },
-        dataType: 'json',
-        success: function(data) {
-            console.log("Success\ndata: " + data)
-            console.log("Group: " + data['_embedded']['groups'][0]['name']);
-        },
-        error: function(data) {
-            console.log("Error\ndata: " + data)
-        }
-    });
-
-    console.log("token test");
     $.ajax({
         url: tokenURL,
         type: 'post',
@@ -36,21 +16,19 @@ function getAnnotationsToken() {
             username: username,
             password: password
         },
-//        headers: {
-//            'Authorization': 'Basic ' + btoa(client + ":" + clientPassword),
-//            'Content-Type': 'application/x-www-form-urlencoded'
-//        },
         beforeSend: function(xhr) {
             xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client + ':' + clientPassword));
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//            xhr.setRequestHeader('Accept', 'application/json');
         },
         dataType: 'json',
         success: function(data) {
-            console.log(data)
-            console.log(data['access_token']);
-            console.log("expires in: " + data['expires_in']);
-            console.log("expires at: " + new Date(((new Date()).getTime() + data['expires_in']*1000)));
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (data['expires_in']*1000));
+            document.cookie = "access_token="+data['access_token']+"; expires=" + expires.toGMTString();
+            sessionStorage.setItem('refresh_token', data['refresh_token']);
+        },
+        error: function(data) {
+            console.log("Error fetching annotations access token\ndata: " + data);
         }
     });
 }
