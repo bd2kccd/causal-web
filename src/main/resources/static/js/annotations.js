099a1686c6d9206ae1b32ae393ab251cd0f61912
Mark Silvis <masilvis22@gmail.com>
2016-05-25 23:18:55 -0400

    Website now fetches tokens from Annotations API
    CORS issues have been resolved

--- src/main/resources/static/js/annotations.js
+++ src/main/resources/static/js/annotations.js
@@ -1,20 +1,25 @@
+/**
+ * @author Mark Silvis (marksilvis@pitt.edu)
+ */
+
 const apiURL = "http://localhost:8000/api";
 const tokenURL = apiURL+"/oauth/token";
+const client = "causal-web";
+const clientPassword = "";
 
-function getAnnotationsToken() {
-    const client = "causal-web";
-    const clientPassword = "";
+/**
+ * Fetch refresh and access tokens from Annotations API
+ */
+function requestAnnotationsTokens() {
     const grantType = "password";
-    const username = document.getElementById("login").username.value;
-    const password = document.getElementById("login").password.value;
 
     $.ajax({
         url: tokenURL,
         type: 'post',
         data: {
             grant_type: grantType,
-            username: username,
-            password: password
+            username: document.getElementById("login").username.value,
+            password: document.getElementById("login").password.value
         },
         beforeSend: function(xhr) {
             xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client + ':' + clientPassword));
@@ -22,13 +27,75 @@ function getAnnotationsToken() {
         },
         dataType: 'json',
         success: function(data) {
-            var expires = new Date();
-            expires.setTime(expires.getTime() + (data['expires_in']*1000));
-            document.cookie = "access_token="+data['access_token']+"; expires=" + expires.toGMTString();
-            sessionStorage.setItem('refresh_token', data['refresh_token']);
+            storeAccessToken(data['access_token'], data['expires_in']);
+            storeRefreshToken(data['refresh_token']);
         },
         error: function(data) {
-            console.log("Error fetching annotations access token\ndata: " + data);
+            console.log("Error fetching tokens\ndata: " + data);
+        }
+    });
+}
+
+/**
+ * Save access token in cookie
+ * @param  {string} token access token
+ * @param  {int} expires  token expiration in seconds
+ */
+function storeAccessToken(token, expires) {
+    var date = new Date();
+    date.setTime(date.getTime() + expires*1000);
+    document.cookie = "access_token=" + token + "; expires=" + date.toGMTString();
+}
+
+/**
+ * Get access token from cookies
+ * @return {string} access token
+ */
+function getAccessToken() {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + 'access_token' + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+/**
+ * Save refresh token in session storage
+ * @param  {string} token refresh token
+ */
+function storeRefreshToken(token) {
+    sessionStorage.setItem('refresh_token', token);
+}
+
+/**
+ * Get refresh token from session storage
+ * @return {string} refresh token
+ */
+function getRefreshToken() {
+    return sessionStorage.getItem('refresh_token');
+}
+
+/**
+ * Fetch new access token using refresh token
+ */
+function requestAccessToken() {
+    const grantType = "refresh_token";
+
+    $.ajax({
+        url: tokenURL,
+        type: 'post',
+        data: {
+            grant_type: grantType,
+            refresh_token: getRefreshToken();
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client + ':' + clientPassword));
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        },
+        dataType: 'json',
+        success: function(data) {
+            storeAccessToken(data['access_token'], data['expires_in']);
+        },
+        error: function(data) {
+            console.log("Error fetching access token\ndata: " + data);
         }
     });
 }
