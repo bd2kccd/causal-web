709940f6ee065bf55e81e2519a86ea5ab03674fc
Kevin Bui <kvb2univpitt@gmail.com>
2016-10-06 15:03:06 -0400

    Fixed collapsing panels.

--- src/main/resources/static/js/algo/algorithm.js
+++ src/main/resources/static/js/algo/algorithm.js
@@ -1,37 +1,3 @@
-$(document).on('click', '.panel-heading span.clickable', function (e) {
-    var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-    } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-    }
-});
-
-function sync() {
-    var selection = '';
-    $('#dataset option:selected').each(function () {
-        if (selection !== '') {
-            selection += ',';
-        }
-        selection += $(this).val();
-    });
-    $('#datasetSelection').text(selection);
-}
-
-$(document).ready(function () {
-    $("#continuous").prop("disabled", true);
-    $('#dataset').multiselect({
-        includeSelectAllOption: true,
-        enableFiltering: true,
-        disableIfEmpty: true,
-        buttonWidth: '100%',
-        onChange: function (option, checked) {
-            $('#datasetSelection').text($('.multiselect-selected-text').text());
-        }
-    });
-    sync();
+$(document).on('click', '#optJvm', function (e) {
+    collapseAction(this);
 });
