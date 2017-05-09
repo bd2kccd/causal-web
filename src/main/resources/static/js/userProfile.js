9a85e416860fb08e81195c69ef399332a8ba5c46
Kevin Bui <kvb2univpitt@gmail.com>
2017-05-09 02:09:15 -0400

    Updated user profile view.

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    $("#view_info_panel").show();
-    $("#edit_info_panel").hide();
-
     $("#edit_info").click(function () {
         $("#view_info_panel").hide();
         $("#edit_info_panel").show();
